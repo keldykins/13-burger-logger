93ea4a5ff2c86873535c622e2f97f30a23ef07fc
keldykins <kelcundy@gmail.com>
2020-09-18 14:30:48 -0600

    saving work

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,15 @@
-### Schema
-
+-- Schema
+DROP DATABASE IF EXISTS burgerloggerDB;
 CREATE DATABASE burgerloggerDB;
-USE cat_db;
+USE burgerloggerDB;
 
 CREATE TABLE everyburger
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burgers varchar(150) NOT NULL,
-	PRIMARY KEY (id)
+	id int NOT NULL
+	AUTO_INCREMENT,
+	name varchar
+	(150) NOT NULL,
+	devoured boolean NOT NULL DEFAULT FALSE,
+	PRIMARY KEY
+	(id)
 );
