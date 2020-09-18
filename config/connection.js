5e76848b868309bd299cef9bdbb616d8dc219acb
keldykins <kelcundy@gmail.com>
2020-09-18 14:37:31 -0600

    saving work

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,17 @@ var connection = mysql.createConnection({
   database: "burgerloggerDB",
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "burgerloggerDB",
+  });
+};
+
 // Make connection.
 connection.connect(function (err) {
   if (err) {
